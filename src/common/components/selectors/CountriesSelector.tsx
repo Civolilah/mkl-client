42d578ae40b430e19772403b15c156677dbc4115
Civolilah <abedinhalilovic12345@gmail.com>
2025-08-23 14:37:13 +0200

    Added countries list localization

--- src/common/components/selectors/CountriesSelector.tsx
+++ src/common/components/selectors/CountriesSelector.tsx
@@ -10,9 +10,36 @@
 
 import { useMemo } from 'react';
 
-import { countries } from 'countries-list';
+import countries from 'i18n-iso-countries';
+import bs from 'i18n-iso-countries/langs/bs.json';
+import de from 'i18n-iso-countries/langs/de.json';
+import en from 'i18n-iso-countries/langs/en.json';
+import es from 'i18n-iso-countries/langs/es.json';
+import fr from 'i18n-iso-countries/langs/fr.json';
+import hi from 'i18n-iso-countries/langs/hi.json';
+import hr from 'i18n-iso-countries/langs/hr.json';
+import it from 'i18n-iso-countries/langs/it.json';
+import pt from 'i18n-iso-countries/langs/pt.json';
+import sr from 'i18n-iso-countries/langs/sr.json';
+import tr from 'i18n-iso-countries/langs/tr.json';
+import zh from 'i18n-iso-countries/langs/zh.json';
+import { useAtomValue } from 'jotai';
 
+import { userCompanyAtom } from '@components/general/PrivateRoute';
 import { SelectStaticField } from '@components/index';
+
+countries.registerLocale(en);
+countries.registerLocale(bs);
+countries.registerLocale(de);
+countries.registerLocale(fr);
+countries.registerLocale(it);
+countries.registerLocale(es);
+countries.registerLocale(pt);
+countries.registerLocale(tr);
+countries.registerLocale(zh);
+countries.registerLocale(hi);
+countries.registerLocale(hr);
+countries.registerLocale(sr);
 
 interface Props {
   label?: string;
@@ -31,14 +58,23 @@ const CountriesSelector = ({
   onValueChange,
   errorMessage,
 }: Props) => {
+  const userCompany = useAtomValue(userCompanyAtom);
+
   const countryOptions = useMemo(() => {
-    return Object.entries(countries)
-      .map(([code, country]) => ({
+    const countryNames = countries.getNames(
+      userCompany?.preference.language || 'en',
+      { select: 'official' }
+    );
+
+    return Object.entries(countryNames)
+      .map(([code, name]) => ({
         value: code,
-        label: country.native,
+        label: name,
       }))
-      .sort((a, b) => a.label.localeCompare(b.label));
-  }, []);
+      .sort((a, b) =>
+        a.label.localeCompare(b.label, userCompany?.preference.language || 'en')
+      );
+  }, [userCompany]);
 
   return (
     <SelectStaticField
